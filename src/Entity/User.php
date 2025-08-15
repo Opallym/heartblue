da1225faa30be57156cde9d8ef7f82460d1e2673
Opallym <gondremaelle@gmail.com>
2025-08-15 14:32:22 +0200

    maj

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use App\Entity\Activity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cet email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
