cc4d8a9b771148c6cb59d9b7e23723ea720ed15e
Opallym <gondremaelle@gmail.com>
2025-08-15 13:41:58 +0200

    politique de confidentialiter

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LegalController extends AbstractController
+{
+    #[Route('/politique-de-confidentialite', name: 'app_privacy_policy')]
+    public function privacyPolicy(): Response
+    {
+        return $this->render('privacy_policy/privacy_policy.html.twig');
+    }
+}
